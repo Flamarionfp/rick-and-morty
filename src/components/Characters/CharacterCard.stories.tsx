cc2b90ed5148682a3ec97c3eeceb13b8e22d57b6
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-05-22 17:42:49 -0300

    feat: install storybook in project

---
+++ src/components/Characters/CharacterCard.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CharacterCard } from './CharacterCard';
+
+export default {
+  title: 'CharacterCard',
+  component: CharacterCard,
+} as ComponentMeta<typeof CharacterCard>;
+
+const Template: ComponentStory<typeof CharacterCard> = (args) => <CharacterCard {...args} />;
+
+export const _CharacterCard = Template.bind({});
+
+_CharacterCard.args = {
+  name: 'Rick',
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  species: 'Human',
+  status: 'Alive',
+  gender: 'Male'
+};
